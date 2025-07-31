ce9fccf41f0c2c6a2bb4bfde397424ccc0a725ac
Vipipi <konamiv3@hotmail.com>
2025-07-30 18:53:53 -0700

    add mono_direct mode

---
+++ src/mono_direct.cpp
@@ -0,0 +1,160 @@
+/*
+
+A bare-bones example node demonstrating the use of the Monocular mode in ORB-SLAM3
+
+Author: Vipipi 
+Date: 07/30/2025
+
+REQUIREMENTS
+* Make sure to set path to your workspace in common.hpp file
+
+*/
+
+//* Includes
+#include "ros2_orb_slam3/mono_direct_common.hpp"
+
+//* Constructor
+MonocularDirectMode::MonocularDirectMode() :Node("mono_direct_node_cpp")
+{
+    // Declare parameters to be passsed from command line
+    // https://roboticsbackend.com/rclcpp-params-tutorial-get-set-ros2-params-with-cpp/
+    
+    //* Find path to home directory
+    homeDir = getenv("HOME");
+    // std::cout<<"Home: "<<homeDir<<std::endl;
+    
+    // std::cout<<"VLSAM NODE STARTED\n\n";
+    RCLCPP_INFO(this->get_logger(), "\nORB-SLAM3-V1 NODE STARTED");
+
+    this->declare_parameter("node_name_arg", "not_given"); // Name of this agent 
+    this->declare_parameter("voc_file_arg", "file_not_set"); // Needs to be overriden with appropriate name  
+    this->declare_parameter("settings_file_path_arg", "file_path_not_set"); // path to settings file  
+    
+    //* Watchdog, populate default values
+    nodeName = "not_set";
+    vocFilePath = "file_not_set";
+    settingsFilePath = "file_not_set";
+
+    //* Populate parameter values
+    rclcpp::Parameter param1 = this->get_parameter("node_name_arg");
+    nodeName = param1.as_string();
+    
+    rclcpp::Parameter param2 = this->get_parameter("voc_file_arg");
+    vocFilePath = param2.as_string();
+
+    rclcpp::Parameter param3 = this->get_parameter("settings_file_path_arg");
+    settingsFilePath = param3.as_string();
+
+    // rclcpp::Parameter param4 = this->get_parameter("settings_file_name_arg");
+    
+  
+    //* HARDCODED, set paths
+    if (vocFilePath == "file_not_set" || settingsFilePath == "file_not_set")
+    {
+        pass;
+        vocFilePath = homeDir + "/" + packagePath + "orb_slam3/Vocabulary/ORBvoc.txt.bin";
+        settingsFilePath = homeDir + "/" + packagePath + "orb_slam3/config/Monocular/isaac-sim.yaml";
+    }
+
+    // std::cout<<"vocFilePath: "<<vocFilePath<<std::endl;
+    // std::cout<<"settingsFilePath: "<<settingsFilePath<<std::endl;
+    
+    
+    //* DEBUG print
+    RCLCPP_INFO(this->get_logger(), "nodeName %s", nodeName.c_str());
+    RCLCPP_INFO(this->get_logger(), "voc_file %s", vocFilePath.c_str());
+    RCLCPP_INFO(this->get_logger(), "settings_file_path %s", settingsFilePath.c_str());
+
+    // Directly subscribe to /camera/rgb/image_raw
+    subImgMsgName = "/camera/rgb/image_raw";
+    subImgMsg_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+        subImgMsgName, 1, std::bind(&MonocularDirectMode::Img_callback, this, _1)
+    );
+
+    // Set default timestep to 0.0
+    timeStep = 0.0;
+
+    // Initialize VSLAM immediately with the default settings file
+    initializeVSLAM(settingsFilePath);
+
+    RCLCPP_INFO(this->get_logger(), "Subscribed to %s and initialized VSLAM.", subImgMsgName.c_str());
+}
+
+//* Destructor
+MonocularDirectMode::~MonocularDirectMode()
+{   
+    // Stop all threads
+    // Call method to write the trajectory file
+    // Release resources and cleanly shutdown
+    if (pAgent) {
+        pAgent->Shutdown();
+    }
+    pass;
+}
+
+//* Method to bind an initialized VSLAM framework to this node
+void MonocularDirectMode::initializeVSLAM(std::string& configString){
+    // Watchdog, if the paths to vocabulary and settings files are still not set
+    if (vocFilePath == "file_not_set" || settingsFilePath == "file_not_set")
+    {
+        RCLCPP_ERROR(get_logger(), "Please provide valid voc_file and settings_file paths");       
+        rclcpp::shutdown();
+    } 
+
+    // If configString is a path to a .yaml, use as is, else append .yaml
+    if (configString.size() < 5 || configString.substr(configString.size() - 5) != ".yaml") {
+        settingsFilePath = configString + ".yaml";
+    } else {
+        settingsFilePath = configString;
+    }
+
+    RCLCPP_INFO(this->get_logger(), "Path to settings file: %s", settingsFilePath.c_str());
+    
+    // NOTE if you plan on passing other configuration parameters to ORB SLAM3 Systems class, do it here
+    // NOTE you may also use a .yaml file here to set these values
+    sensorType = ORB_SLAM3::System::MONOCULAR; 
+    enablePangolinWindow = true; // Shows Pangolin window output
+    enableOpenCVWindow = true; // Shows OpenCV window output
+    
+    pAgent = new ORB_SLAM3::System(vocFilePath, settingsFilePath, sensorType, enablePangolinWindow);
+    std::cout << "MonocularDirectMode node initialized" << std::endl; // TODO needs a better message
+}
+
+//* Callback to process image message and run SLAM node
+void MonocularMode::Img_callback(const sensor_msgs::msg::Image& msg)
+{
+    // Initialize
+    cv_bridge::CvImagePtr cv_ptr; //* Does not create a copy, memory efficient
+    
+    //* Convert ROS image to openCV image
+    try
+    {
+        //cv::Mat im =  cv_bridge::toCvShare(msg.img, msg)->image;
+        cv_ptr = cv_bridge::toCvCopy(msg); // Local scope
+        
+        // DEBUGGING, Show image
+        // Update GUI Window
+        // cv::imshow("test_window", cv_ptr->image);
+        // cv::waitKey(3);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        RCLCPP_ERROR(this->get_logger(),"Error reading image");
+        return;
+    }
+    
+    // Use ROS time stamp as timeStep if available, else fallback to 0.0
+    double img_time = 0.0;
+    if (msg.header.stamp.sec != 0 || msg.header.stamp.nanosec != 0) {
+        img_time = rclcpp::Time(msg.header.stamp).seconds();
+    } else {
+        img_time = timeStep;
+    }
+
+    //* Perform all ORB-SLAM3 operations in Monocular mode
+    //! Pose with respect to the camera coordinate frame not the world coordinate frame
+    Sophus::SE3f Tcw = pAgent->TrackMonocular(cv_ptr->image, img_time); 
+    
+    //* An example of what can be done after the pose w.r.t camera coordinate frame is computed by ORB SLAM3
+    //Sophus::SE3f Twc = Tcw.inverse(); //* Pose with respect to global image coordinate, reserved for future use
+}
